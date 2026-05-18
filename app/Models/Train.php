b87b5d0973ba57a5d2e5ac1b0fb2d45226a51ca1
giovannimazzi <gioelor@libero.it>
2026-05-18 20:27:40 +0200

    add train table and style

--- app/Models/Train.php
+++ app/Models/Train.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Train extends Model
 {
-    //
+    protected $casts=[
+        "departure_date" => 'date',
+        "departure_time" => 'datetime',
+        "arrival_time" => 'datetime',
+    ];
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,11 +4,71 @@
 
 @section('content')
     <div class="container py-4">
-        ORARI
-        <ul>
-            @foreach ($trains as $train)
-                <li>{{$train}}</li>
-            @endforeach
-        </ul>
+        <h2>Treni in partenza dal {{today()->format('d/m/Y')}}</h2>
+        <div class="table-responsive train-table">
+
+            <table class="table table-striped table-hover align-middle">
+
+                <thead class="table-dark sticky-top">
+                    <tr>
+                        <th class="text-warning fw-normal">Azienda</th>
+                        <th class="text-warning fw-normal">Partenza</th>
+                        <th class="text-warning fw-normal">Arrivo</th>
+                        <th class="text-warning fw-normal">Data</th>
+                        <th class="text-warning fw-normal">Ora Partenza</th>
+                        <th class="text-warning fw-normal">Ora Arrivo</th>
+                        <th class="text-warning fw-normal">Codice</th>
+                        <th class="text-warning fw-normal">Carrozze</th>
+                        <th class="text-warning fw-normal">Info Orario</th>
+                        <th class="text-warning fw-normal">Stato</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @foreach ($trains as $train)
+                        <tr>
+                            <td>{{ $train->company }}</td>
+
+                            <td>{{ $train->departure_station }}</td>
+
+                            <td>{{ $train->arrival_station }}</td>
+
+                            <td>
+                                {{ $train->departure_date->format('d/m/Y') }}
+                            </td>
+
+                            <td>{{ $train->departure_time->format('H:i')  }}</td>
+
+                            <td>{{ $train->arrival_time->format('H:i')  }}</td>
+
+                            <td>{{ $train->train_code }}</td>
+
+                            <td>{{ $train->carriages_number }}</td>
+
+                            <td>
+                                @if (!$train->is_cancelled)
+                                    @if ($train->is_on_time)
+                                        <span class="badge bg-success">In orario</span>
+                                    @else
+                                        <span class="badge bg-warning text-dark">Ritardo</span>
+                                    @endif
+                                @endif
+                            </td>
+
+                            <td>
+                                @if ($train->is_cancelled)
+                                    <span class="badge bg-danger">Cancellato</span>
+                                @else
+                                    <span class="badge bg-primary">Attivo</span>
+                                @endif
+                            </td>
+
+                        </tr>
+                    @endforeach
+                </tbody>
+
+            </table>
+
+        </div>
     </div>
 @endsection
