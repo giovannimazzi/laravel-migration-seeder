114ae48d28a91178cf47dd6890c8f89131fcee4d
giovannimazzi <gioelor@libero.it>
2026-05-19 16:56:29 +0200

    fix homepage view

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <nav class="py-4">
             <a href="{{route("pages.home")}}" class="text-decoration-none  text-light">
-                <h1 class="my-0">🚂Tabellone Treni</h1>
+                <h1 class="my-0">Tabellone Treni</h1>
             </a>
         </nav>
     </div>
